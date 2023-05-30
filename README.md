85c0642ac785bcdef00d37e5df0dbe0cbf7fbbf4
AnthonyToons7 <anthonyschuurmans4@gmail.com>
2023-05-30 15:03:42 +0200

    Added more comments and explanation

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Totally safe to use
 
 It coaches you to stop using else if over and over and over again.
 
+You can also say that it convinces you to use GitHub more...?
+
 Sometimes, it's just better to use that switch case instead of else if.
 
 It triggers the second you finish that second f
--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,22 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand('noifelse--.stopType', () => {
+    // Register your current document as your current active editor
     const editor = vscode.window.activeTextEditor;
     if (editor) {
       const document = editor.document;
       const text = document.getText();
+      // Regex so it searches all "else if"s
       const regex = /else\s+if/g;
+      // Match if the regex is the same or is present in the document
       const matches = text.match(regex);
 
       if (matches && matches.length >= 2) {
         vscode.commands.executeCommand("editor.action.selectAll"); // Select everything
         vscode.commands.executeCommand("deleteLeft"); // press Backspace
         vscode.commands.executeCommand("workbench.action.files.save"); // press Ctrl+S to save
-        vscode.window.showErrorMessage("You fool, you have used more than one else if, and this is your punishment.");
+        // Display message in the corner of the screen
+        vscode.window.showErrorMessage("You fool, moron, you absolute bafoon, you have used more than one else if, and this is your punishment. You better have this project on your git, because you're not getting this one back.");
       }
     }
   });
@@ -25,6 +29,7 @@ export function activate(context: vscode.ExtensionContext) {
     const activeEditor = vscode.window.activeTextEditor;
     if (activeEditor && event.document === activeEditor.document) {
       const text = event.document.getText();
+      // Check again
       const regex = /else\s+if/g;
       const matches = text.match(regex);
 
